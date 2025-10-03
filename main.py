41e06cea059491ffcc13daa1cb66ec56221bb086
Connor McGrail <cmcgr39@gmail.com>
2025-10-02 22:02:34 -0400

    I cant remember what I changed tbh

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from game import Game
-
-running = True
 game = Game()
 
-while running:
+while True:
     user_play = input('Welcome to slots! Type "play" to play! ')
+
     if user_play == "play":
         print(f"You have {game.tokens} tokens!")
         user_bet = int(input("Enter a bet: "))
+
+        if user_bet > game.tokens:
+            print("You don't have enough tokens!")
+            continue
+
         game.make_bet(user_bet)
         game.spin()
         print(game.result)
         game.check_win()
 
+        if game.tokens <= 0:
+            print("You are out of tokens!")
+            quit()
+
+    elif user_play == "tokens":
+        print(f"You have {game.tokens} tokens!")
+
+    elif user_play == "quit":
+        quit()
 
     else:
         continue
-
-
